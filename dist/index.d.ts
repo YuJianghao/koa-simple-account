d3e7f06bf7dfd9587e5a7802fd109130706c732e
winwin2011 <yjh2011@live.com>
2021-11-04 07:37:23 +0800

    chore: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="koa" />
 /// <reference types="koa__router" />
+import { IAuthInfo } from "./storage";
 interface IConfig {
     path: string;
     secret: string;
@@ -10,5 +11,6 @@ interface IConfig {
 export declare function createSimpleAccount(config: IConfig): {
     middleware: import("koa").Middleware<import("koa").DefaultState, import("koa").Context & import("koa").DefaultContext & import("@koa/router").RouterParamContext<import("koa").DefaultState, import("koa").DefaultContext>, any>;
     auth: (type?: "access" | "refresh") => (ctx: import("koa").Context, next: import("koa").Next) => Promise<void>;
+    setAuthInfo: (newInfo?: Partial<IAuthInfo>) => void;
 };
 export {};
